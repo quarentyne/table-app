552840eb3f5d4fd16ca880c77b6544a5944bfa0e
quarentyne <peresada1997@gmail.com>
2022-12-29 17:50:03 +0200

    fixed car grid-template

--- src/modules/car/components/car/styles.ts
+++ src/modules/car/components/car/styles.ts
@@ -16,7 +16,7 @@ export const CarsTableInner = styled.ul`
   margin-bottom: 15px;
 
   @media screen and (min-width: ${screenSizes.tablet}px){
-    grid-template-areas: "id name model mark year plate status action";
+    grid-template-areas: "id name mark model plate year status action";
     grid-template-columns: 0.4fr 1.5fr 1fr 1fr 1.3fr 0.7fr 1.2fr 0.6fr;
     gap: 10px;
     border: none;
