046fa3a61c09def3af1e6544aa3cb1a9e6e2cfb2
quarentyne <peresada1997@gmail.com>
2023-01-20 16:18:59 +0200

    rename config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { RouterConfig } from './router/Config';
+import { RouterConfig } from './router/cConfig';
 import { Loading } from './shared/components/Loading/Loading';
 import { GlobalStyle } from './styles';
 
